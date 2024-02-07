4bae552926423a9acde253fe32ec0cd66d55186f
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2024-02-07 07:59:22 -0300

    Aula 1 - até 1h03m

--- src/main/java/br/com/victorjunior/certification_nlw/PrimeiraController.java
+++ src/main/java/br/com/victorjunior/certification_nlw/PrimeiraController.java
@@ -1,6 +1,7 @@
 package br.com.victorjunior.certification_nlw;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrimeiraController {
   
   @GetMapping("/retornarPrimeiraController")
-  public String retornoPrimeiraController() {
-    return "Criando minha primeira controller";
+  public Usuario retornoPrimeiraController() {
+    var usuario = new Usuario("Victor", 28);
+    return usuario;
   }
+
+  @PostMapping("/meuPrimeiroPost")
+  public String primeiroPost() {
+    return "Meu primeiro POST";
+  }
+
+  record Usuario(String nome, int idade) {}
 
 }
