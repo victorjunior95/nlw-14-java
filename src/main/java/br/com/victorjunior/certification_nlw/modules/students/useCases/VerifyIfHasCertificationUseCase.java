7e32afc212908f3358e6c02dbb56fd087dcb2d8d
Victor Junior da Silva Ramos <victorjr.muchacho@gmail.com>
2024-02-14 11:12:37 -0300

    Aula 2 - até 1h04 min. - desenvolvimento do módulo 'questions'

--- src/main/java/br/com/victorjunior/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/br/com/victorjunior/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,17 +1,26 @@
 // Equivalente à camada de Service
 package br.com.victorjunior.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.victorjunior.certification_nlw.modules.students.dto.VerifyIfHasCertificationDTO;
+import br.com.victorjunior.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
+
+  @Autowired
+  private CertificationStudentRepository certificationStudentRepository;
   
   public boolean execute(VerifyIfHasCertificationDTO dto) {
-    if(dto.getEmail().equals("victorjunior@gmail.com") && dto.getTechnology().equals("JAVA")) {
+    var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+    if (!result.isEmpty()) {
       return true;
     }
+    // if(dto.getEmail().equals("victorjunior@gmail.com") && dto.getTechnology().equals("JAVA")) {
+    //   return true;
+    // }
     return false;
   }
 }
